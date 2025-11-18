ba3366ee3b036f93ca0b8c1580e69a8a84c425fd
marcinreis <reismarcio143@gmail.com>
2025-11-18 11:19:08 -0300

    adiçao nodemon, adiçao mysql2 e conexao node.js com o banco de dados.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
+//importar modulo express
+
 const express = require('express')
 const app = express()
 const port = 3000
+
+//importar modulo sql
+const sql = require('mysql2')
+//configuraçao de conexao
+const conexao = sql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1,2,3,4,',
+  database: 'projeto'  
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
